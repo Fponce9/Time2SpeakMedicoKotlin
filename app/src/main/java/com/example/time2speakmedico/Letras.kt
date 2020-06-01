0080a22273a457caa467a36d7e1cf3eb3e4cc4fd
Fernando Ponce <exminaruto@gmail.com>
2020-06-01 15:18:06 -0500

    Creacion de Repo y Activities Login, Letras, Palabra y Feedback del doctor

---
+++ app/src/main/java/com/example/time2speakmedico/Letras.kt
@@ -0,0 +1,11 @@
+package com.example.time2speakmedico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Letras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letras)
+    }
+}
